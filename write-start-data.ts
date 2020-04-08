a2f954d9beefc95530e6e316d4a40bed2dbcdca8
Enoumy <enoumy@gmail.com>
2020-04-07 23:49:42 -0600

    Started implementing starter data functions

---
+++ write-start-data.ts
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const sqlite3 = require("sqlite3").verbose();
+
+function executeCommands(db_file: string, sql_file: string): void {
+  let db = new sqlite3.Database(db_file);
+  const schema = fs.readFileSync(sql_file).toString();
+
+  let commands = schema.toString().split(";");
+  db.serialize(() => {
+    db.run("BEGIN TRANSACTION;");
+
+    for (let i = 0; i < commands.length; i++) {
+      console.log(commands[i] + ";");
+      db.run(commands[i] + ";", err => {
+        if (err) console.log(err);
+      });
+    }
+
+    db.run("COMMIT;");
+  });
+
+  db.close();
+}
+
+executeCommands("test.db", "./dummy-data.sql");
